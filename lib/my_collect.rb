f862aaaba4b8deda7c696d5cc405d9b33cfc4141
vedransehovic <vedran.sehovic@gmail.com>
2020-04-20 12:41:54 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+# writing a method my_collect that will act as a collect method in an array
+def my_collect (array)
+    counter = 0
+    new_array = []
+    while counter<array.length 
+        new_array << yield(array[counter])
+        counter += 1
+    end
+    new_array
+end
 
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(names) do |name|
+  name.split(" ").first
+end
